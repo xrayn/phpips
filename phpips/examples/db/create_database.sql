ea99b5eca8052d99329fa003ec1c36695a3d5d16
Andre Rein <ra@ra23.net>
2010-10-08 17:27:21 +0000

    added sql example

---
+++ phpips/examples/db/create_database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [loggertable] (
+[id] INTEGER  NOT NULL PRIMARY KEY,
+[time] TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+[affected_tags] VARCHAR(256)  NULL,
+[impact] INTEGER  NOT NULL,
+[session_impact] INTEGER  NOT NULL
+);
